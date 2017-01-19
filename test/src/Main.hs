a21ab824b01ca9825586b005a8ac5a32fad330a9
LinneaAndersson <Linneamaria10@gmail.com>
2017-01-19 19:28:00 +0100

    Save tmp in file

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -5,14 +5,22 @@ import Data.Maybe
 import Data.Either
 import GHC.IO.Handle
 import Tip.Parser
+import System.IO
+
+tip_file :: FilePath
+tip_file = "tip_file.smt2"
 
 main :: IO()
 main = do
     (_,Just handle,_,_) <- createProcess(
        proc "tip-ghc" ["Int.hs"] ){ cwd = Just "../../tools/examples"  } 
     tip_string <- hGetContents handle 
-    case parse tip_string of 
-        Left x          -> print $ "Failed to create theory: " ++ x
-        Right x         -> print x 
+    writeFile tip_file tip_string
+    theory_either <- parseFile tip_file
+    case theory_either of 
+        Left x  -> do 
+                    print $ "Failed to create theory: " ++ x
+                            
+        Right theory -> print theory 
     return ()
 
