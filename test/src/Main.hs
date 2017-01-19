b5cec48da5eed98f9daaadcb1dc3b1ef267386ae
LinneaAndersson <Linneamaria10@gmail.com>
2017-01-19 16:12:14 +0100

    created relative path

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -5,11 +5,14 @@ import Data.Maybe
 import GHC.IO.Handle
 
 main :: IO()
-main = withFile "$D/Master_Thesis/repositories/FunInd/test/out.smt2" WriteMode $ \out -> do
-        r <- createProcess(
-                --proc "tip-ghc" ["/home/linnea/Master_thesis/tools/examples/Int.hs"] )
-                proc "tip-ghc" ["$D/Master_Thesis/repositories/tools/examples/Int.hs"] ) 
-                { std_out = UseHandle out } 
---        kkk <- hGetContents h 
---        print kkk
+main = do
+        (_,Just h,_,_) <- createProcess(
+           proc "tip-ghc" ["Int.hs"] ){ cwd = Just "../../tools/examples"  } 
+        kkk <- hGetContents h 
+        print kkk
         return ()
+
+
+--withFile "$D/Master_Thesis/repositories/FunInd/test/out.smt2" WriteMode $ \out -> do
+ --proc "tip-ghc" ["$D/Master_Thesis/repositories/tools/examples/Int.hs"] ) 
+                --{ std_out = UseHandle out } 
