f43c8b9a3dad501854a4b2a462a28d5737d1b5e7
AemW <andreas.wahlstrom@hotmail.com>
2017-01-26 16:20:59 +0100

    added tip-types and removed .cabal from gitignore

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -5,19 +5,25 @@ import Data.Maybe
 import Data.Either
 import GHC.IO.Handle
 import Tip.Parser
+import Tip.Types
+import Tip.Core (theoryGoals)
 import System.IO
 
 tip_file :: FilePath
-tip_file = "tip_file.smt2"
+tip_file = out_path "tip_file.smt2"
 
 theory_file :: FilePath
-theory_file = "theory_file.txt"
+theory_file = out_path "theory_file.txt"
 
 prop_file :: FilePath
-prop_file = "prop.txt"
+prop_file = out_path "prop.txt"
 
 example_path :: FilePath
-example_path = "../../../../../../examples"
+example_path = "./examples"
+--example_path"../../../../../../examples"
+
+out_path :: FilePath -> FilePath
+out_path = (++) "./out/"
 
 main :: IO()
 main = do
@@ -36,33 +42,51 @@ main = do
             tip_string <- hGetContents handle
             writeFile tip_file tip_string
             -- parsing TIP into a theory
-            theory_either <- parseFile tip_file
+            theory <- readTheory tip_file
+            print $ "theory created!"
+            writeFile theory_file $ show theory
 
-            -- check whether the parsing succeded
-            case theory_either of
-                Left x  -> print $ "Failed to create theory: " ++ x
-                Right theory -> do
-                    -- created a theory
-                    print $ "theory created!"
-                    writeFile theory_file $ show theory
+            -- start a external Process for tip-spec, generating conjectures
+            -- for the parsed theory
+            (_,proc2,_,p_id) <- createProcess( proc "tip-spec" [tip_file] )
+                { cwd = Just ".", std_out = CreatePipe  }
+            waitForProcess p_id
 
-                    -- start a external Process for tip-spec, generating conjectures
-                    -- for the parsed theory
-                    (_,proc2,_,p_id) <- createProcess( proc "tip-spec" [tip_file] )
-                        { cwd = Just ".", std_out = CreatePipe  }
-                    waitForProcess p_id
+            -- check whether tip-spec succeded
+            case proc2 of
+                Just handle -> do
+                    -- created conjectures
+                    print "properties created!"
+                    prop_string <- hGetContents handle
+                    writeFile prop_file prop_string
+                    print $ "done writing to: "
+                            ++ tip_file ++", "
+                            ++ theory_file ++ ", "
+                            ++ prop_file
 
-                    -- check whether tip-spec succeded
-                    case proc2 of
-                        Just handle -> do
-                            -- created conjectures
-                            print "properties created!"
-                            prop_string <- hGetContents handle
-                            writeFile prop_file prop_string
-                            print $ "done writing to: "
-                                    ++ tip_file ++", "
-                                    ++ theory_file ++ ", "
-                                    ++ prop_file
-                        Nothing -> print "Failed to create properties"
+                Nothing -> print "Failed to create properties"
         Nothing -> print "could not find directory or file"
     return ()
+
+readTheory :: FilePath -> IO (Theory Id)
+readTheory fp = do
+  theory_either <- parseFile fp
+  -- check whether the parsing succeded
+  case theory_either of
+      Left x  -> fail $ "Failed to create theory: " ++ x
+      Right theory -> return theory
+
+test :: IO()
+test = do
+  theory <- readTheory prop_file
+  let (goals, assume) = theoryGoals theory
+  return ()
+
+
+{-
+jukebox :: IO()
+jukebox = do
+  (_,proc1,_,p_id) <-  createProcess( proc "jukebox" ["Int.hs"] )
+              { cwd = Just example_path, std_out = CreatePipe  }
+  waitForProcess p_id
+-}
