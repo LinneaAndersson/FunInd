5d315761c5e04dea3135cd16dec92d01b29c3eb1
AemW <andreas.wahlstrom@hotmail.com>
2017-01-29 17:58:55 +0100

    cleaned code and added 'correct' passes

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -28,50 +28,30 @@ out_path = (++) "./out/"
 
 main :: IO()
 main = do
-    
     -- query for input file
     (a:args) <- getArgs
     let (path, file) = splitFileName a
- 
+
     -- start a external Process for tip-ghc, translating a haskell file
     -- into smt2 (tip-format).
-    (_,proc1,_,p_id) <-  createProcess( proc "tip-ghc" [file] )
-                { cwd = Just path, std_out = CreatePipe  }
-    waitForProcess p_id
+    tip_string <- run_process "tip-ghc" path [file]
+    print "tip created!"
+    writeFile tip_file tip_string
 
-    -- check whether tip-ghc finished correctly
-    case proc1 of
-        Just handle -> do
-            -- Sucessfully translated file
-            print "tip created!"
-            tip_string <- hGetContents handle
-            writeFile tip_file tip_string
+    -- parsing TIP into a theory
+    theory <- readTheory tip_file
+    print $ "theory created!"
+    writeFile theory_file $ show $ theory
 
-            -- parsing TIP into a theory
-            theory <- readTheory tip_file
-            print $ "theory created!"
-            writeFile theory_file $ show $ theory
-
-            -- start a external Process for tip-spec, generating conjectures
-            -- for the parsed theory
-            (_,proc2,_,p_id) <- createProcess( proc "tip-spec" [tip_file] )
-                { cwd = Just ".", std_out = CreatePipe  }
-            waitForProcess p_id
-
-            -- check whether tip-spec succeded
-            case proc2 of
-                Just handle -> do
-                    -- created conjectures
-                    print "properties created!"
-                    prop_string <- hGetContents handle
-                    writeFile prop_file prop_string
-                    print $ "done writing to: "
-                            ++ tip_file ++", "
-                            ++ theory_file ++ ", "
-                            ++ prop_file
-                    test
-                Nothing -> print "Failed to create properties"
-        Nothing -> print "could not find directory or file"
+    -- created conjectures
+    prop_string <- run_process "tip-spec" "." [tip_file]
+    print "properties created!"
+    writeFile prop_file prop_string
+    print $ "done writing to: "
+            ++ tip_file ++", "
+            ++ theory_file ++ ", "
+            ++ prop_file
+    test
     return ()
 
 readTheory :: FilePath -> IO (Theory Id)
@@ -83,42 +63,50 @@ readTheory fp = do
       Right theory -> return theory
 
 passes :: Theory Id -> [Theory Id]
-passes = freshPass (runPasses [SkolemiseConjecture])
-
+passes = freshPass (runPasses
+        [ SkolemiseConjecture, TypeSkolemConjecture
+          , Monomorphise False
+          , LambdaLift, AxiomatizeLambdas
+          , SimplifyGently, CollapseEqual, RemoveAliases
+          , SimplifyGently, Monomorphise False, IfToBoolOp, CommuteMatch
+          , SimplifyGently, LetLift, SimplifyGently, AxiomatizeFuncdefs2
+          , SimplifyGently, AxiomatizeDatadecls
+        ])
+{-
+      [  TypeSkolemConjecture, Monomorphise False
+          , SimplifyGently, LambdaLift, AxiomatizeLambdas, Monomorphise False
+          , SimplifyGently, CollapseEqual, RemoveAliases
+          , SimplifyGently
+          , AxiomatizeFuncdefs2
+          , RemoveMatch -- in case they appear in conjectures
+          , SkolemiseConjecture
+          , NegateConjecture
+      ])
+-}
 test :: IO()
 test = do
   theory <- readTheory prop_file
   let goal = selectConjecture 0 theory
   --let goal = axiomatizeFuncdefs theory'
-  let goal' = head $ freshPass (runPasses      
-        [  TypeSkolemConjecture, Monomorphise False
-            , SimplifyGently, LambdaLift, AxiomatizeLambdas, Monomorphise False
-            , SimplifyGently, CollapseEqual, RemoveAliases
-            , SimplifyGently
-            , AxiomatizeFuncdefs2
-            , RemoveMatch -- in case they appear in conjectures
-            , SkolemiseConjecture
-            , NegateConjecture
-        ]) goal
+  let goal' = head $ passes ({-head $ freshPass (induction [0])-} goal)
   let goal'' = ppTheory goal'
   writeFile (out_path "goal1.smt2") $ show $ goal
   writeFile (out_path "goal2.smt2") $ show $ goal'
   writeFile (out_path "goal.smt2") $ show $ goal''
-  jukebox (out_path "goal1.smt2")
+  jukebox (out_path "goal.smt2")
   return ()
 
 
-jukebox :: FilePath -> IO()
-jukebox source = do
-  (_,proc1,_,p_id) <-  createProcess( proc jb ["fof",source] )
-              { cwd = Just ".", std_out = CreatePipe  }
-  waitForProcess p_id
-  case proc1 of
-    Nothing -> fail $ "could not run jukebox: "
-    Just handle -> do
-        content <- hGetContents handle
-        print $ "jukbox done: " ++ content
-  return ()
-    where 
-        jb = ".stack-work/install/x86_64-linux/lts-7.7/8.0.1/bin/jukebox"
-    
+jukebox :: FilePath -> IO String
+jukebox source = fmap ((++) "jukbox done: ") $ run_process jb "." ["fof", source]
+        where jb = ".stack-work/install/x86_64-linux/lts-7.7/8.0.1/bin/jukebox"
+
+-- name -> cwd -> optional args -> output
+run_process :: String -> FilePath -> [String] -> IO String
+run_process name path ops = do
+    (_,proc,_,p_id) <- createProcess( proc name ops )
+        { cwd = Just path, std_out = CreatePipe  }
+    waitForProcess p_id
+    case proc of
+        Nothing     -> fail $ "Error: Could not run '" ++ name ++ "'"
+        Just handle -> hGetContents handle
