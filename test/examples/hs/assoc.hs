d97ab313727c9d4086f861944b6e62c36a6a6c10
LinneaAndersson <Linneamaria10@gmail.com>
2017-02-06 13:18:15 +0100

    changed assoc example

--- test/examples/hs/assoc.hs
+++ test/examples/hs/assoc.hs
@@ -9,5 +9,5 @@ p :: Nat -> Nat -> Nat
 p Z b = b
 p (S a) b = S (p a b)
 
-prop_a_1 :: Nat -> Nat -> Nat -> Prop
-prop_a_1 a b c =  p a (p b c) === p (p a b) c
+prop_a_1 :: Nat -> Nat -> Prop
+prop_a_1 b c = forAll (\a ->  p a (p b c) === p (p a b) c ==> forAll (\a' -> a'=== S a ==>  p a' (p b c) === p (p a' b) c ))
