843645e5930eb18d47be80e87eede3ab3f343bc4
AemW <andreas.wahlstrom@hotmail.com>
2017-04-17 12:33:20 +0200

    prop bench file

--- test/examples/benchmarks/Prop.hs
+++ test/examples/benchmarks/Prop.hs
@@ -1,58 +1,89 @@
 
-import Tip
-import Prelude hiding ((++),map)
-
-(++) :: [Prop] -> [Prop] -> [Prop]
-[]      ++ bs = b
-(a:as)  ++ bs = a : (as ++ bs)
-
-map :: (Prop -> Bool) -> [Prop] -> [Bool]
-map _ []     = []
-map f (a:as) = f a : map f as 
-
-map1 :: (Prop -> Prop) -> [Prop] -> [Prop]
-map1 _ []     = []
-map1 f (a:as) = f a : map1 f as 
+import qualified Tip as Tip
+import Prelude hiding ((++), map, elem, and, Bool(..), not)
 
 -- trees
 
 data Prop
   = Not Prop
-  | And [Prop]
+  | And PropList
   | Var Nat
- deriving ( Eq, Ord, Show )
+ deriving ( Eq, Ord )
 
 type Nat = Int -- or whatever
 
+data Bool = True | False
+    deriving Eq
+
+data PropList 
+    = PNil
+    | PCons Prop PropList
+    deriving (Eq, Ord)
+
+data NatList
+    = NNil
+    | NCons Nat NatList
+    deriving Eq
+
+data BoolList
+    = BNil
+    | BCons Bool BoolList
+    deriving Eq
+
+(++) :: PropList -> PropList -> PropList
+PNil      ++ bs = bs
+(PCons a as)  ++ bs = PCons a (as ++ bs)
+
+map :: NatList -> PropList -> BoolList
+map _ PNil     = BNil
+map mod (PCons a as) = BCons (eval mod a) $ map mod as 
+
+map1 :: PropList -> PropList
+map1 PNil     = PNil
+map1 (PCons a as) = PCons (simp a) $ map1 as 
+
+elem :: Nat -> NatList -> Bool
+elem x (NCons y ys) = if x == y then True else x `elem` ys 
+
+
+and :: BoolList -> Bool
+and BNil            = True
+and (BCons True bs)    = (and bs) 
+and (BCons False bs)   = False
+
+not :: Bool -> Bool
+not True = False
+not False = True
+
 -- eval
 
-eval :: [Nat] -> Prop -> Bool
+eval :: NatList -> Prop -> Bool
 eval mod (Not p)  = not (eval mod p)
-eval mod (And ps) = and (map (eval mod) ps)
+eval mod (And ps) = and (map mod ps)
 eval mod (Var x)  = x `elem` mod
 
 -- simp
 
 simp :: Prop -> Prop
 simp (Not p)  = simpNot (simp p)
-simp (And ps) = simpAnd (map1 simp ps)
+simp (And ps) = simpAnd (map1 ps)
 simp (Var x)  = Var x
 
 simpNot :: Prop -> Prop
 simpNot (Not p) = p
 simpNot p       = Not p
 
-simpAnd :: [Prop] -> Prop
+simpAnd :: PropList -> Prop
 simpAnd ps = makeAnd (flatAnd ps)
 
-makeAnd :: [Prop] -> Prop
-makeAnd [p] = p
+makeAnd :: PropList -> Prop
+makeAnd (PCons p PNil) = p
 makeAnd ps  = And ps
 
-flatAnd :: [Prop] -> [Prop]
-flatAnd (And ps : qs) = ps ++ flatAnd qs
-flatAnd (p      : qs) = p : flatAnd qs
-flatAnd []            = []
+flatAnd :: PropList -> PropList
+flatAnd (PCons (And ps) qs) = ps ++ flatAnd qs
+flatAnd (PCons p qs)         = PCons p $ flatAnd qs
+flatAnd PNil                 = PNil
 
 -- QuickCheck
 {-
@@ -71,6 +102,7 @@ instance Arbitrary Prop where
                 | otherwise = n-1
          sequence [ arb n' | i <- [1..k] ]
 -}
-prop_Simp ts p =
-  eval ts p === eval ts (simp p)
+--prop_simp :: NatList -> Prop -> Tip.Prop
+prop_simp ts p =
+  eval ts p Tip.=== eval ts (simp p)
 
